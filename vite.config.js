c598f4f4a74e53f87a6bcbc36b7377f576a76205
p!i5748zX <ali.abd.allatif@perla-tech.com>
2023-08-21 14:06:54 -0700

    movie club

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 
 
 import { defineConfig } from "vite";
@@ -16,12 +10,10 @@ export default defineConfig({
     "process.env": {},
   },
   plugins: [react()],
+  base:"/MovieClub/"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // build: {
-  //   chunkSizeWarningLimit: 5000,
-  // },
 });
